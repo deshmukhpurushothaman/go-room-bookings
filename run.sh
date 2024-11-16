f647f331bd485c14074fddf13c85123adfa3a9a9
Deshmukh P <deshmukhpurushothaman@gmail.com>
2024-11-16 14:47:20 +0530

    command line params for envs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 go build -o go-room-bookings ./cmd/web
-./go-room-bookings
+./go-room-bookings -dbname=go-room-bookings -dbuser=postgres -dbpass=deshmukh123 -cache=false -production=false
